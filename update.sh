057b07aeb98e60fe5016b02d5d33b1f12b0b6ef7
Adam Birse <adambirse@gmail.com>
2024-04-17 14:52:39 +0100

    script refactor

--- update.sh
+++ update.sh
@@ -1,27 +1,43 @@
 #!/bin/bash
 
+get_outdated_packages() {
+    # Get a list of outdated packages
+    # outdated=$(npm outdated --json | jq -r 'keys[]')
+    # yarn gives error
+    # jq: error (at <stdin>:1): Cannot index string with string "body"
+    # but it seems to work ???
+        yarn outdated --json | jq -r '.data.body[][0]'
+}
 
-# Get the blacklist from package.json
-blacklist=$(jq -r '.["package-updater"].black_list[]' package.json)
+get_blacklist() {
+    local filename=$1
+    jq -r '.["package-updater"].black_list[]' $filename
+}
+
+update_packages() {
+    local blacklist=$1
+    local outdated=$2
+
+    for package in $outdated
+    do
+        # If the package is not in the blacklist
+        if ! [[ " ${blacklist[@]} " =~ " ${package} " ]]
+        then
+            # Update the package
+            echo updating $package
+            yarn upgrade $package --latest
+        fi 
+    done
+}
+
+blacklist=$(get_blacklist "package.json")
 echo blacklist $blacklist
-# Get a list of outdated packages
-# outdated=$(npm outdated --json | jq -r 'keys[]')
-# yarn gives error
-# jq: error (at <stdin>:1): Cannot index string with string "body"
-# but it seems to work ???
-outdated=$(yarn outdated --json | jq -r '.data.body[].[0]')
 
 
+outdated=$(get_outdated_packages)
 echo outdated $outdated
-# # Loop through the outdated packages
-for package in $outdated
-do
-    # If the package is not in the blacklist
-    if ! [[ " ${blacklist[@]} " =~ " ${package} " ]]
-    then
-        # Update the package
-        echo updating $package
-        # npm update $package
-        yarn upgrade $package --latest
-    fi 
-done
+
+
+
+
+update_packages "$blacklist" "$outdated"
