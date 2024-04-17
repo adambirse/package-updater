96a41d71232920bdd565db38824c0fcabf4a6525
Adam Birse <adambirse@gmail.com>
2024-04-17 15:28:42 +0100

    script refactor

--- update.sh
+++ update.sh
@@ -1,43 +1,50 @@
 #!/bin/bash
 
 get_outdated_packages() {
-    # Get a list of outdated packages
-    # outdated=$(npm outdated --json | jq -r 'keys[]')
-    # yarn gives error
-    # jq: error (at <stdin>:1): Cannot index string with string "body"
-    # but it seems to work ???
-        yarn outdated --json | jq -r '.data.body[][0]'
+    # Get a list of outdated packages using yarn
+    yarn outdated --json | jq -r '.data.body[][0]'
 }
 
 get_blacklist() {
     local filename=$1
-    jq -r '.["package-updater"].black_list[]' $filename
+    # Retrieve the blacklist from the package.json file
+    jq -r '.["package-updater"].black_list[]' "$filename"
+}
+
+update_package() {
+    local package=$1
+    # Update the specified package to the latest version
+    echo "Updating $package"
+    yarn upgrade "$package" --latest
 }
 
 update_packages() {
-    local blacklist=$1
-    local outdated=$2
+    local blacklist=($1)
+    local outdated=($2)
 
-    for package in $outdated
+    for package in "${outdated[@]}"
     do
         # If the package is not in the blacklist
-        if ! [[ " ${blacklist[@]} " =~ " ${package} " ]]
-        then
-            # Update the package
-            echo updating $package
-            yarn upgrade $package --latest
+        if [[ ! " ${blacklist[@]} " =~ " ${package} " ]]; then
+            update_package "$package"
         fi 
     done
 }
 
-blacklist=$(get_blacklist "package.json")
-echo blacklist $blacklist
+main() {
+    local package_json="package.json"
 
+    # Get the blacklist from package.json
+    local blacklist=$(get_blacklist "$package_json")
+    echo "Blacklist: ${blacklist[@]}"
 
-outdated=$(get_outdated_packages)
-echo outdated $outdated
+    # Get the outdated packages
+    local outdated=$(get_outdated_packages)
+    echo "Outdated packages: ${outdated[@]}"
 
+    # Update the packages
+    update_packages "$blacklist" "$outdated"
+}
 
-
-
-update_packages "$blacklist" "$outdated"
+# Entry point
+main
